218be55b5f6273c93f05e88a9ec4c0fba32e84c0
dk <dk@10.97.21.221>
2012-12-04 11:57:18 +0100

    JPA VIRKER

--- 2.SemesterProjektJPA/src/model/Mellemvarelager.java
+++ 2.SemesterProjektJPA/src/model/Mellemvarelager.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 import service.Service;
 
