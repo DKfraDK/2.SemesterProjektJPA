ffc7c68c8908a5477c9c299a66563b51da93a177
dk <dk@10.97.21.98>
2012-12-06 15:58:00 +0100

    ENDELIG UDGAVE

--- 2.SemesterProjektJPA/src/UnitTest/RelationsTest.java
+++ 2.SemesterProjektJPA/src/UnitTest/RelationsTest.java
@@ -15,134 +15,305 @@ import service.Service;
 
 public class RelationsTest {
 
+	// //////////////////TEST I MELLEMVARELAGER//////////////////////
 	@Test
-	public void test() {
-		//fail("Not yet implemented");
-		System.out.println("ekstra test");
-	}
-	////////////////////TEST I MELLEMVARELAGER//////////////////////
-	@Test
-	public void testAddMellemvare(){
+	public void testAddMellemvare() {
 		System.out.println("Test addMellemvare");
 		Delbehandling dHvile = new Delbehandling("Hvile periode", 1, 2, 8);
 		Behandling b1 = new Behandling("Skumbehandling");
 		Service.addDelbehandlingTilBehandling(b1, dHvile);
-		Produkttype p1 = new Produkttype("pSkumBanan",b1);
+		Produkttype p1 = new Produkttype("pSkumBanan", b1);
 		Mellemvarelager m = Mellemvarelager.getInstance("Mellemvare");
-		Mellemvare mv = new Mellemvare("007",p1,0);
+		Mellemvare mv = new Mellemvare("007", p1, 0);
 		m.addMellemvare(mv);
 		assertTrue(m.getMellemvarer().contains(mv));
-		
+
 	}
-////////////////////TEST I MELLEMVARELAGER//////////////////////
+
+	// //////////////////TEST I MELLEMVARELAGER//////////////////////
 	@Test
-	public void testRemoveMellemvare(){
+	public void testRemoveMellemvare() {
 		System.out.println("Test removeMellemvare");
 		Delbehandling dHvile = new Delbehandling("Hvile periode", 1, 2, 8);
 		Behandling b1 = new Behandling("Skumbehandling");
 		Service.addDelbehandlingTilBehandling(b1, dHvile);
-		Produkttype p1 = new Produkttype("pSkumBanan",b1);
+		Produkttype p1 = new Produkttype("pSkumBanan", b1);
 		Mellemvarelager m = Mellemvarelager.getInstance("Mellemvare");
-		Mellemvare mv = new Mellemvare("007",p1,0);
-		
+		Mellemvare mv = new Mellemvare("007", p1, 0);
+
 		m.addMellemvare(mv);
-		
+
 		m.removeMellemvare(mv);
 		assertFalse(m.getMellemvarer().contains(mv));
 	}
-	
+
 	@Test
-////////////////////TEST I MELLEMVARE//////////////////////
-	public void testGetProdukttype_og_testAtMellemvareFaarTilknyttetEnProdukttypeVedOprettelse(){
+	// //////////////////TEST I MELLEMVARE//////////////////////
+	public void testGetProdukttype_og_testAtMellemvareFaarTilknyttetEnProdukttypeVedOprettelse() {
 		System.out.println("Test getProdukttype");
 		Delbehandling dHvile = new Delbehandling("Hvile periode", 1, 2, 8);
 		Behandling b1 = new Behandling("Skumbehandling");
 		Service.addDelbehandlingTilBehandling(b1, dHvile);
-		Produkttype p1 = new Produkttype("pSkumBanan",b1);
-		Mellemvare mv = new Mellemvare("007",p1,0);
-		
-		assertSame(p1,mv.getProdukttype());
-		
+		Produkttype p1 = new Produkttype("pSkumBanan", b1);
+		Mellemvare mv = new Mellemvare("007", p1, 0);
+
+		assertSame(p1, mv.getProdukttype());
+
 	}
-////////////////////TEST I MELLEMVARE//////////////////////
+
+	// //////////////////TEST I MELLEMVARE//////////////////////
 	@Test
-	public void testSetProdukttype(){
+	public void testSetProdukttype() {
 		System.out.println("Test setProdukttype");
 		Delbehandling dHvile = new Delbehandling("Hvile periode", 1, 2, 8);
 		Behandling b1 = new Behandling("Skumbehandling");
 		Service.addDelbehandlingTilBehandling(b1, dHvile);
-		Produkttype p1 = new Produkttype("pSkumBanan",b1);
-		Produkttype p2 = new Produkttype("pPtaerte",b1);
-		Mellemvare mv = new Mellemvare("007",p1,0);
-		
+		Produkttype p1 = new Produkttype("pSkumBanan", b1);
+		Produkttype p2 = new Produkttype("pPtaerte", b1);
+		Mellemvare mv = new Mellemvare("007", p1, 0);
+
 		mv.setProdukttype(p2);
 		assertTrue(mv.getProdukttype().equals(p2));
-		
+
 	}
-////////////////////TEST I MELLEMVARE//////////////////////
+
+	// //////////////////TEST I MELLEMVARE//////////////////////
 	@Test
-	public void testCreateToerretid_Og_getToerretider(){
+	public void testCreateToerretid_Og_getToerretider() {
 		System.out.println("Test CreateToerretid_Og_getToerretider");
 		Delbehandling dHvile1 = new Delbehandling("Hvile periode", 1, 2, 8);
 		Delbehandling dHvile2 = new Delbehandling("Hvile periode", 1, 2, 8);
 		Behandling b1 = new Behandling("Skumbehandling");
 		Service.addDelbehandlingTilBehandling(b1, dHvile1);
 		Service.addDelbehandlingTilBehandling(b1, dHvile2);
-		Produkttype p1 = new Produkttype("pSkumBanan",b1);
-		Mellemvare mv = new Mellemvare("007",p1,0);
-		
+		Produkttype p1 = new Produkttype("pSkumBanan", b1);
+		Mellemvare mv = new Mellemvare("007", p1, 0);
+
 		Toerretid t = mv.createToerretid(0);
 		assertTrue(mv.getToerretider().contains(t));
-		
+
 	}
-////////////////////TEST I MELLEMVARE//////////////////////
+
+	// //////////////////TEST I MELLEMVARE//////////////////////
 	@Test
-	public void testDeleteToerretid(){
+	public void testDeleteToerretid() {
 		System.out.println("Test DeleteToerretid");
 		Delbehandling dHvile1 = new Delbehandling("Hvile periode", 1, 2, 8);
 		Delbehandling dHvile2 = new Delbehandling("Hvile periode", 1, 2, 8);
 		Behandling b1 = new Behandling("Skumbehandling");
 		Service.addDelbehandlingTilBehandling(b1, dHvile1);
 		Service.addDelbehandlingTilBehandling(b1, dHvile2);
-		Produkttype p1 = new Produkttype("pSkumBanan",b1);
-		Mellemvare mv = new Mellemvare("007",p1,0);
+		Produkttype p1 = new Produkttype("pSkumBanan", b1);
+		Mellemvare mv = new Mellemvare("007", p1, 0);
 		Toerretid t = mv.createToerretid(0);
-		
-		
+
 		assertTrue(mv.getToerretider().contains(t));
 		mv.deleteToerretid(t);
 		assertTrue(!mv.getToerretider().contains(t));
-		
-		
+
 	}
-////////////////////TEST I MELLEMVARE//////////////////////
+
+	// //////////////////TEST I MELLEMVARE//////////////////////
 	@Test
-	public void testGetNaesteDelbehandling(){
+	public void testGetNaesteDelbehandling() {
 		Delbehandling dHvile1 = new Delbehandling("Hvile periode", 1, 2, 8);
 		Delbehandling dHvile2 = new Delbehandling("Hvile periode", 1, 2, 8);
 		Behandling b1 = new Behandling("Skumbehandling");
 		Service.addDelbehandlingTilBehandling(b1, dHvile1);
 		Service.addDelbehandlingTilBehandling(b1, dHvile2);
-		Produkttype p1 = new Produkttype("pSkumBanan",b1);
-		Mellemvare mv = new Mellemvare("007",p1,0);
-	
-		
-		
+		Produkttype p1 = new Produkttype("pSkumBanan", b1);
+		Mellemvare mv = new Mellemvare("007", p1, 0);
+
 		assertTrue(dHvile2.equals(mv.getNaesteDelbehandling()));
 	}
-////////////////////TEST I MELLEMVARE//////////////////////
+
+	// //////////////////TEST I MELLEMVARE//////////////////////
 	@Test
-	public void testGetSidsteDelbehandling(){
+	public void testGetSidsteDelbehandling() {
 		Delbehandling dHvile1 = new Delbehandling("Hvile periode", 1, 2, 8);
 		Delbehandling dHvile2 = new Delbehandling("Hvile periode", 1, 2, 8);
 		Behandling b1 = new Behandling("Skumbehandling");
 		Service.addDelbehandlingTilBehandling(b1, dHvile1);
 		Service.addDelbehandlingTilBehandling(b1, dHvile2);
-		Produkttype p1 = new Produkttype("pSkumBanan",b1);
-		Mellemvare mv = new Mellemvare("007",p1,0);
+		Produkttype p1 = new Produkttype("pSkumBanan", b1);
+		Mellemvare mv = new Mellemvare("007", p1, 0);
 		Toerretid t = mv.createToerretid(0);
-		
+
 		assertTrue(dHvile2.equals(mv.getSidsteDelbehandling()));
 	}
+
+	// //////////////////TEST I MELLEMVARELAGER//////////////////////
+	// ////////////GetNæsteMellemvareTilDelbehandling//////
+	@Test
+	public void testEnkeltvare() {
+		Delbehandling dHvile1 = new Delbehandling("Hvile periode", 1, 2, 8);
+		Delbehandling dHvile2 = new Delbehandling("Hvile periode", 1, 2, 8);
+		Behandling b1 = new Behandling("Skumbehandling");
+		Service.addDelbehandlingTilBehandling(b1, dHvile1);
+		Service.addDelbehandlingTilBehandling(b1, dHvile2);
+		Produkttype p1 = new Produkttype("pSkumBanan", b1);
+		Mellemvarelager m = Mellemvarelager.getInstance("Mellemvare");
+		m.clearLager();
+		m.setDage(2);
+		Mellemvare mv1 = new Mellemvare("008", p1, 0);
+		m.addMellemvare(mv1);
+		System.out.println("Print af lager" + m.getMellemvarer());
+		assertEquals(mv1, m.getNaesteMellemvareTilBehandling());
+	}
+
+	// //////////////////TEST I MELLEMVARELAGER//////////////////////
+	// ////////////GetNæsteMellemvareTilDelbehandling//////
+	@Test
+	public void testForMax() {
+		Delbehandling dHvile1 = new Delbehandling("Hvile periode1", 1, 2, 6);
+		Delbehandling dHvile2 = new Delbehandling("Hvile periode2", 1, 2, 4);
+		Delbehandling dHvile3 = new Delbehandling("Hvile periode3", 1, 2, 3);
+		Behandling b1 = new Behandling("Skumbehandling");
+		Behandling b2 = new Behandling("SkumNu");
+		Service.addDelbehandlingTilBehandling(b1, dHvile1);
+		Service.addDelbehandlingTilBehandling(b1, dHvile2);
+		Service.addDelbehandlingTilBehandling(b2, dHvile3);
+		Service.addDelbehandlingTilBehandling(b2, dHvile2);
+		Produkttype p1 = new Produkttype("pSkumBanan", b1);
+		Produkttype p2 = new Produkttype("pPtart", b2);
+		Mellemvarelager m = Mellemvarelager.getInstance("Mellemvare");
+		m.clearLager();
+		m.setDage(3);
+		Mellemvare mv1 = new Mellemvare("008", p1, 0);
+		Mellemvare mv2 = new Mellemvare("009", p2, 0);
+		m.addMellemvare(mv1);
+		m.addMellemvare(mv2);
+		System.out.println("næste behandling" + mv1.getSidsteDelbehandling());
+		System.out.println("Print af lager" + m.getMellemvarer());
+		assertEquals(mv2, m.getNaesteMellemvareTilBehandling());
+	}
+	
+	// //////////////////TEST I MELLEMVARELAGER//////////////////////
+		// ////////////GetNæsteMellemvareTilDelbehandling//////
+		@Test
+		public void testIdeal() {
+			Delbehandling dHvile1 = new Delbehandling("Hvile periode1", 1, 2, 6);
+			Delbehandling dHvile2 = new Delbehandling("Hvile periode2", 1, 2, 4);
+			Delbehandling dHvile3 = new Delbehandling("Hvile periode3", 1, 3, 12);
+			Behandling b1 = new Behandling("Skumbehandling");
+			Behandling b2 = new Behandling("SkumNu");
+			Service.addDelbehandlingTilBehandling(b1, dHvile1);
+			Service.addDelbehandlingTilBehandling(b1, dHvile2);
+			Service.addDelbehandlingTilBehandling(b2, dHvile3);
+			Service.addDelbehandlingTilBehandling(b2, dHvile2);
+			Produkttype p1 = new Produkttype("pSkumBanan", b1);
+			Produkttype p2 = new Produkttype("pPtart", b2);
+			Mellemvarelager m = Mellemvarelager.getInstance("Mellemvare");
+			m.clearLager();
+			m.setDage(3);
+			Mellemvare mv1 = new Mellemvare("008", p1, 0);
+			Mellemvare mv2 = new Mellemvare("009", p2, 0);
+			m.addMellemvare(mv1);
+			m.addMellemvare(mv2);
+			System.out.println("næste behandling" + mv1.getSidsteDelbehandling());
+			System.out.println("Print af lager" + m.getMellemvarer());
+			assertEquals(mv2, m.getNaesteMellemvareTilBehandling());
+		}
+		// //////////////////TEST I MELLEMVARELAGER//////////////////////
+		// ////////////GetNæsteMellemvareTilDelbehandling//////
+		@Test
+		public void testEftermin() {
+			Delbehandling dHvile1 = new Delbehandling("Hvile periode1", 4, 4, 6);
+			Delbehandling dHvile2 = new Delbehandling("Hvile periode2", 3, 4, 5);
+			Delbehandling dHvile3 = new Delbehandling("Hvile periode3", 3, 4, 7);
+			Behandling b1 = new Behandling("Skumbehandling");
+			Behandling b2 = new Behandling("SkumNu");
+			Service.addDelbehandlingTilBehandling(b1, dHvile1);
+			Service.addDelbehandlingTilBehandling(b1, dHvile2);
+			Service.addDelbehandlingTilBehandling(b2, dHvile3);
+			Service.addDelbehandlingTilBehandling(b2, dHvile2);
+			Produkttype p1 = new Produkttype("pSkumBanan", b1);
+			Produkttype p2 = new Produkttype("pPtart", b2);
+			Mellemvarelager m = Mellemvarelager.getInstance("Mellemvare");
+			m.clearLager();
+			m.setDage(3);
+			Mellemvare mv1 = new Mellemvare("008", p1, 0);
+			Mellemvare mv2 = new Mellemvare("009", p2, 0);
+			m.addMellemvare(mv1);
+			m.addMellemvare(mv2);
+			System.out.println("næste behandling" + mv1.getSidsteDelbehandling());
+			System.out.println("Print af lager" + m.getMellemvarer());
+			assertEquals(mv2, m.getNaesteMellemvareTilBehandling());
+		}
+		
+		// //////////////////TEST I MELLEMVARELAGER//////////////////////
+		// ////////////GetNæsteMellemvareTilDelbehandling//////
+		@Test
+		public void testEns() {
+			Delbehandling dHvile1 = new Delbehandling("Hvile periode1", 1, 2, 6);
+			Delbehandling dHvile2 = new Delbehandling("Hvile periode2", 1, 2, 6);
+			Delbehandling dHvile3 = new Delbehandling("Hvile periode3", 1, 2, 6);
+			Behandling b1 = new Behandling("Skumbehandling");
+			Behandling b2 = new Behandling("SkumNu");
+			Service.addDelbehandlingTilBehandling(b1, dHvile1);
+			Service.addDelbehandlingTilBehandling(b1, dHvile2);
+			Service.addDelbehandlingTilBehandling(b2, dHvile3);
+			Service.addDelbehandlingTilBehandling(b2, dHvile2);
+			Produkttype p1 = new Produkttype("pSkumBanan", b1);
+			Produkttype p2 = new Produkttype("pPtart", b2);
+			Mellemvarelager m = Mellemvarelager.getInstance("Mellemvare");
+			m.clearLager();
+			m.setDage(3);
+			Mellemvare mv1 = new Mellemvare("008", p1, 0);
+			Mellemvare mv2 = new Mellemvare("009", p2, 0);
+			m.addMellemvare(mv1);
+			m.addMellemvare(mv2);
+			System.out.println("næste behandling" + mv1.getSidsteDelbehandling());
+			System.out.println("Print af lager" + m.getMellemvarer());
+			assertEquals(mv1, m.getNaesteMellemvareTilBehandling());
+		}
+		// //////////////////TEST I MELLEMVARELAGER//////////////////////
+		// ////////////GetNæsteMellemvareTilDelbehandling//////
+		@Test
+		public void testForklar() {
+			Delbehandling dHvile1 = new Delbehandling("Hvile periode1", 4, 6, 8);
+			Delbehandling dHvile2 = new Delbehandling("Hvile periode2", 4, 6, 8);
+			Delbehandling dHvile3 = new Delbehandling("Hvile periode3", 4, 6, 8);
+			Behandling b1 = new Behandling("Skumbehandling");
+			Behandling b2 = new Behandling("SkumNu");
+			Service.addDelbehandlingTilBehandling(b1, dHvile1);
+			Service.addDelbehandlingTilBehandling(b1, dHvile2);
+			Service.addDelbehandlingTilBehandling(b2, dHvile3);
+			Service.addDelbehandlingTilBehandling(b2, dHvile2);
+			Produkttype p1 = new Produkttype("pSkumBanan", b1);
+			Produkttype p2 = new Produkttype("pPtart", b2);
+			Mellemvarelager m = Mellemvarelager.getInstance("Mellemvare");
+			m.clearLager();
+			m.setDage(3);
+			Mellemvare mv1 = new Mellemvare("008", p1, 0);
+			Mellemvare mv2 = new Mellemvare("009", p2, 0);
+			m.addMellemvare(mv1);
+			m.addMellemvare(mv2);
+			System.out.println("næste behandling" + mv1.getSidsteDelbehandling());
+			System.out.println("Print af lager" + m.getMellemvarer());
+			assertEquals(null, m.getNaesteMellemvareTilBehandling());
+		}
+		
+		// //////////////////TEST I MELLEMVARELAGER//////////////////////
+		// ////////////GetNæsteMellemvareTilDelbehandling//////
+		@Test
+		public void testTom() {
+			Delbehandling dHvile1 = new Delbehandling("Hvile periode1", 1, 2, 6);
+			Delbehandling dHvile2 = new Delbehandling("Hvile periode2", 1, 2, 4);
+			Delbehandling dHvile3 = new Delbehandling("Hvile periode3", 1, 2, 3);
+			Behandling b1 = new Behandling("Skumbehandling");
+			Behandling b2 = new Behandling("SkumNu");
+			Service.addDelbehandlingTilBehandling(b1, dHvile1);
+			Service.addDelbehandlingTilBehandling(b1, dHvile2);
+			Service.addDelbehandlingTilBehandling(b2, dHvile3);
+			Service.addDelbehandlingTilBehandling(b2, dHvile2);
+			Produkttype p1 = new Produkttype("pSkumBanan", b1);
+			Produkttype p2 = new Produkttype("pPtart", b2);
+			Mellemvarelager m = Mellemvarelager.getInstance("Mellemvare");
+			m.clearLager();
+			m.setDage(3);
+			System.out.println("Print af lager" + m.getMellemvarer());
+			assertEquals(null, m.getNaesteMellemvareTilBehandling());
+		}
 
 }
--- 2.SemesterProjektJPA/src/model/Mellemvarelager.java
+++ 2.SemesterProjektJPA/src/model/Mellemvarelager.java
@@ -21,13 +21,26 @@ public class Mellemvarelager { //One-to-many Unidirectional
 	private List<Mellemvare> forGamleMellemvarer = new ArrayList<Mellemvare>();
 	private int dage = 0;
 
+	/**
+	 * Private kontruktor for mellemvarelager
+	 * @param navn navnet på mellemvarelageret
+	 */
 	private Mellemvarelager(String navn) {
 		this.navn = navn;
 	}
+	
+	/**
+	 * Nødvendig kontruktor for JPA
+	 */
 	public Mellemvarelager(){
 	
 	}
 
+	/**
+	 * Singleton getter for mellemvarelageret
+	 * @param newnavn navnet på mellemvarelaget
+	 * @return Mellemvarelageret
+	 */
 	public static Mellemvarelager getInstance(String newnavn) {
 		if (uniqueInstance == null) {
 			uniqueInstance = new Mellemvarelager(newnavn);
@@ -35,6 +48,13 @@ public class Mellemvarelager { //One-to-many Unidirectional
 		return uniqueInstance;
 	}
 
+	/**
+	 * Returnerer en oversigt over kritiske mellemvarer, altså mellemvarer
+	 * der har lagt til tørre i længere eller samme tid som deres pågældene delbehandlings
+	 * idealtørretid og mindre eller samme tid som deres pågældenes delbehandlings
+	 * maxtørretid.
+	 * @return En ArrayList<Mellemvare> med alle kritiske mellemvarer.
+	 */
 	public ArrayList<Mellemvare> getOversigtOverKritiskeMellemvarer() {
 		ArrayList<Mellemvare> kritiskeMellemvarer = new ArrayList<Mellemvare>();
 		for (Mellemvare m : mellemvarer) {
@@ -49,10 +69,19 @@ public class Mellemvarelager { //One-to-many Unidirectional
 		return kritiskeMellemvarer;
 	}
 
+	/**
+	 * @return En ArrayList<Mellemvare> med alle færdige mellemvarer.
+	 */
 	public ArrayList<Mellemvare> getFaerdigeMellemvarer() {
 		return new ArrayList<Mellemvare>(faerdigeMellemvarer);
 	}
 
+	/**
+	 * En metode til at finde det antal dage en mellemvare har lagt til tørring
+	 * siden den fik sin sidste delbehandling.
+	 * @param m den mellemvare man vil have tiden på
+	 * @return antal dage mellemvaren har lagt på lager siden sidste delbehandling
+	 */
 	public int getDageTilToerreSidenSidsteDelbehandling(Mellemvare m) {
 		return dage
 				- m.getToerretider().get(m.getToerretider().size() - 1)
@@ -60,6 +89,10 @@ public class Mellemvarelager { //One-to-many Unidirectional
 	}
 
 	/**
+	 * En metode til at finde den næste mellemvare som er klar til behandling.
+	 * Denne findes ved at prioritere mellemvarene efter deres nuværende delbehandlings
+	 * min, ideal og max tid. Så de varer der er igang med en delbehandling som er
+	 * tættest på at blive for gammel bliver returneret først.
 	 * @return Næste mellemvare klar til behandling prioriteret efter
 	 *         mellemvarens delbehandlingers min, ideal og max tørretider.
 	 * @return Null hvis der ikke findes en mellemvare der er klar til en ny
@@ -110,6 +143,12 @@ public class Mellemvarelager { //One-to-many Unidirectional
 		return resultMellemvare;
 	}
 
+	/**
+	 * Hjælpemetode til at finde en liste over alle mellemvarer som er igang med en delbehandling
+	 * der er på dens maksimale tørretid lige nu.
+	 * @return En ArrayList<Mellemvare> som indeholder alle de mellemvarer der har maksimal-
+	 * tørretid på deres nuværende delbehandling.
+	 */
 	private ArrayList<Mellemvare> getMellemvarerMedMaxToerretid() {
 		ArrayList<Mellemvare> resultList = new ArrayList<Mellemvare>();
 		for (Mellemvare m : mellemvarer) {
@@ -119,7 +158,14 @@ public class Mellemvarelager { //One-to-many Unidirectional
 		}
 		return resultList;
 	}
-
+	
+	/**
+	 * Hjælpemetode til at finde en liste over alle mellemvarer som er igang med en delbehandling
+	 * der er på ideal tørretid lige nu.
+	 * @return En ArrayList<Mellemvare> som indeholder alle de mellemvarer hvor idealtørretiden <= 
+	 * den nuværende tid mellemvare har lagt til tørring og maxtørretiden > den nuværende tid 
+	 * mellemvaren har lagt til tørring.
+	 */
 	private ArrayList<Mellemvare> getMellemvarerMedIdealToerretid() {
 		ArrayList<Mellemvare> resultList = new ArrayList<Mellemvare>();
 		for (Mellemvare m : mellemvarer) {
@@ -131,6 +177,13 @@ public class Mellemvarelager { //One-to-many Unidirectional
 		return resultList;
 	}
 
+	/**
+	 * Hjælpemetode til at finde en liste over alle mellemvarer som er igang med en delbehandling
+	 * der er på minimum tørretid lige nu.
+	 * @return En ArrayList<Mellemvare> som indeholder alle de mellemvarer hvor minimum-tørretiden <= 
+	 * den nuværende tid mellemvare har lagt til tørring og ideal-tørretiden > den nuværende tid 
+	 * mellemvaren har lagt til tørring.
+	 */
 	private ArrayList<Mellemvare> getMellemvarerMedMinToerretid() {
 		ArrayList<Mellemvare> resultList = new ArrayList<Mellemvare>();
 		for (Mellemvare m : mellemvarer) {
@@ -144,9 +197,7 @@ public class Mellemvarelager { //One-to-many Unidirectional
 
 	/**
 	 * st¯tte metode til at flytte en mellemvare til fÊrdigvare
-	 * 
-	 * @param m
-	 *            den mellemvarse man ¯nsker at flytte
+	 * @param m den mellemvarse man ¯nsker at flytte
 	 */
 	private void flytTilFaerdigvare(Mellemvare m) {
 		mellemvarer.remove(m);
@@ -154,53 +205,100 @@ public class Mellemvarelager { //One-to-many Unidirectional
 		m.setStatus(Status.FAERDIG);
 	}
 
+	/**
+	 * @return En List<Mellemvare> som er en kopi af alle mellemvarer.
+	 */
 	public List<Mellemvare> getMellemvarer() {
 		return new ArrayList<Mellemvare>(mellemvarer);
 	}
 
+	/**
+	 * En metode til at tilføje en mellemvare til mellemvarer listen.
+	 * Metoden gør ikke noget hvis den mellemvare man forsøger at tilføje
+	 * allerde findes i mellemvarer listen.
+	 * @param mellemvare den mellemvare man vil tilføje
+	 */
 	public void addMellemvare(Mellemvare mellemvare) {
 		if (!mellemvarer.contains(mellemvare)) {
 			mellemvarer.add(mellemvare);
 		}
 	}
 
+	/**
+	 * En metode til at fjerne en mellemvare fra mellemvarer listen.
+	 * Metoden gør ikke noget hvis den mellemvare man forsøger at fjerne
+	 * ikke findes i listen.
+	 * @param mellemvare den mellemvare man vil fjerne
+	 */
 	public void removeMellemvare(Mellemvare mellemvare) {
 		if (mellemvarer.contains(mellemvare)) {
 			mellemvarer.remove(mellemvare);
 		}
 	}
 
+	/**
+	 * @param m den mellemvare man vil finde placeringen på
+	 * @return en int der fortæller hvilken placering mellemvaren har.
+	 */
 	public int getPlacering(Mellemvare m) {
 		return mellemvarer.indexOf(m);
 	}
 
+	/**
+	 * @return en int der fortæller hvor mange dage lageret har været i brug.
+	 */
 	public int getDage() {
 		return dage;
 	}
 
+	/**
+	 * @param dage det antal dage man vil sætte lageret til at have været aktivt i.
+	 */
 	public void setDage(int dage) {
 		this.dage = dage;
 	}
 
+	/**
+	 * Opdaterer mellemvarelagerets lagerbeholdning af mellemvarer, til at matche de
+	 * mellemvarer der findes i DAO'en.
+	 */
 	public void updateLagerBeholdning() {
 		mellemvarer = Service.getMellemvarer();
 	}
 
+	/**
+	 * Fjerner alle varer fra mellemvarelageret.
+	 */
 	public void clearLager() {
 		mellemvarer.clear();
 	}
+	
+	/**
+	 * @param allMellemvarer sætter mellemvarelagerets mellemvare liste til at være den medgivne liste.
+	 */
 	public void setMellemvarer(List<Mellemvare> allMellemvarer) {
 		mellemvarer = allMellemvarer;
 	}
 	
+	
+	/**
+	 * @return en List<Mellemvare> med alle de mellemvarer som er for gamle.
+	 */
 	public List<Mellemvare> getForGamleMellemvareList() {
 		return forGamleMellemvarer;
 	}
 	
+	/**
+	 * @param nyList sætter forGamleMellemvarer til at være den medgivne liste.
+	 */
 	public void setForGamleMellemvareList(ArrayList<Mellemvare> nyList){
 		forGamleMellemvarer = nyList;
 	}
 
+	/**
+	 * @return en int der fortæller hvor mange mellemvare som er blevet for gamle i
+	 * al den tid mellemvarelageret har eksisteret.
+	 */
 	public int getStatistikOverForGamleMellemvarer(){
 		return forGamleMellemvarer.size();
 	}
